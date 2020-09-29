f9b64f600de3162ac27cbbe4107333a131761833
EricsonWillians <ericson.willians@concil.com.br>
2020-09-28 22:29:06 -0300

    Improved project organization

--- _test.py
+++
@@ -1,9 +0,0 @@
-# import sys, os
-
-# from build import src
-
-# speed = 16.0
-# print('UPDATING SPEEED!')
-# src.py_actor.Py_AddSpeed(speed)
-
-GLOBAL_MONSTER_SPEED = 16
--- src/scripting/pyscript/py_init.cpp
+++ src/scripting/pyscript/py_init.cpp
@@ -8,7 +8,7 @@ void py_init() {
     py::scoped_interpreter guard{};
     py::print("Hello, World! GZDoom now runs Python :)"); // use the Python API
 	// py::module sys_path_updater = py::module::import("sys_path_updater");
-	py::module test = py::module::import("_test");
-	double GLOBAL_MONSTER_SPEED = test.attr("GLOBAL_MONSTER_SPEED").cast<double>();
+	py::module modules = py::module::import("modules");
+	double GLOBAL_MONSTER_SPEED = modules.attr("py_actor").attr("GLOBAL_MONSTER_SPEED").cast<double>();
     py_speed = GLOBAL_MONSTER_SPEED;
 }
